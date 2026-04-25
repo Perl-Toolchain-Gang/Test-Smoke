aedff991694c5b71844e162eb6b2d8a80f571da5
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2026-04-25 12:53:16 +0200

    Do not fail test on snap curl

--- lib/Test/Smoke.pm
+++ lib/Test/Smoke.pm
@@ -2,7 +2,7 @@ package Test::Smoke;
 use strict;
 
 use vars qw($conf);
-our $VERSION  = "1.85";
+our $VERSION  = "1.86";
 
 use base 'Exporter';
 our @EXPORT  = qw( $conf &read_config &run_smoke );
